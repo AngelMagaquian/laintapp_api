92bfd941cba971f90c9b1b4e7f5cfb2f128f1f2d
Angel Magaquian <angelmagakhian@gmail.com>
2025-08-18 17:05:53 -0300

    ssl config

--- src/main.ts
+++ src/main.ts
@@ -4,16 +4,23 @@ import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
 import * as express from 'express';
 import { HttpErrorInterceptor } from './common/interceptors/http-error.interceptor';
-
+import * as fs from 'fs';
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const httpsOptions = {
+    key: fs.readFileSync('/etc/letsencrypt/live/nexogroup.ar/privkey.pem'),
+    cert: fs.readFileSync('/etc/letsencrypt/live/nexogroup.ar/fullchain.pem'),
+  };
+
+  const app = await NestFactory.create(AppModule, {
+    httpsOptions,
+  });
 
   // Configuración de CORS
   app.enableCors({
-    origin: ['http://localhost:3000', 'http://168.181.185.21'],
+    origin: ['http://localhost/', 'https://168.181.185.21/', 'https://nexogroup.ar/', 'http://nexogroup.ar','http//168.181.185.21'],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     credentials: true,
-  });
+ });
 
   // Configuración de límite de tamaño para el body
   app.use(express.json({ limit: '50mb' }));
@@ -38,6 +45,9 @@ async function bootstrap() {
 
   // Interceptor de errores global
   app.useGlobalInterceptors(new HttpErrorInterceptor());
+
+  const expressApp = app.getHttpAdapter().getInstance();
+  expressApp.set('trust proxy', 1);
 
   await app.listen(3010);
 }
